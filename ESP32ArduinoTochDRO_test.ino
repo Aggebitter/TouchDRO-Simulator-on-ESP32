d92e0a289efd494c41b226b21eabab3fdb0daf7d
Aggebitter <david.agner@gmail.com>
2018-08-29 20:11:31 +0000

    Now some dirty code for Chinese Calipers

--- ESP32ArduinoTochDRO_test.ino
+++ ESP32ArduinoTochDRO_test.ino
@@ -52,7 +52,7 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 #  Example: P1; and P0;
 #
 #
-# -----	Alot of info fetched from Yuriy Krushelnytskiy and Ryszard Malinowski Arduino code linked below -----
+#	Alot of info gain from Yuriy Krushelnytskiy and Ryszard Malinowski Arduino code linked below
 #
 #   ArduinoDRO + Tach V5.11
 #
@@ -73,6 +73,10 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 
 #define SCALE_X_ENABLED		1
 #define X_CPI				254000
+#define SCALE_X_TYPE		1			/* 0 = Simulation, 1 = Chinese Caliper, 2 = iGaging */
+#define SCALE_X_CLK_PIN		17
+#define SCALE_X_DATA_PIN	16
+
 
 #define SCALE_Y_ENABLED		1
 #define Y_CPI				254000
@@ -111,8 +115,10 @@ int T_StepSize = 100;
 
 BluetoothSerial SerialBT; 
 
-// Define Function Prototypes that use User Types below here or use a .h file
-//
+#if SCALE_X_ENABLED == 1 && SCALE_X_TYPE == 1
+int bit_array[25]; // For storing the data bit. bit_array[0] = data bit 1 (LSB), bit_array[23] = data bit 24 (MSB).
+unsigned long time_now; // For storing the time when the clock signal is changed from HIGH to LOW (falling edge trigger of data output).
+#endif
 
 
 // Define Functions below here or use other .ino or cpp files
@@ -126,8 +132,20 @@ void setup()
 	SerialBT.begin("ESP32testDRO"); //Bluetooth device name
 	Serial.println("The device started, now you can pair it with bluetooth!");
 
+
+#if SCALE_X_ENABLED == 1
+	#if SCALE_X_TYPE == 0
+		X_Readout = random(X_CPI * -10, X_CPI * 10);  // +- 10"
+	#endif
+
+	#if SCALE_X_TYPE == 1
+		pinMode(SCALE_X_CLK_PIN, INPUT);
+		pinMode(SCALE_X_DATA_PIN, INPUT);
+
+	#endif
+#endif
 	// Get some random seed for the axises
-	X_Readout = random(X_CPI * -10, X_CPI * 10);  // +- 10"
+	
 	Y_Readout = random(Y_CPI * -10, Y_CPI * 10);
 	Z_Readout = random(Z_CPI * -10, Z_CPI * 10);
 	W_Readout = random(0, W_CPA * 360 );
@@ -145,38 +163,48 @@ void loop()
 	//Serial.println("X" + String(X_Readout) + ";" + "Y" + String(Y_Readout) + ";" + "Z" + String(Z_Readout) + ";");
 
 #if SCALE_X_ENABLED == 1
-	SerialBT.print("X" + String(X_Readout) + ";" );
-	Serial.print("X" + String(X_Readout) + ";");
-	X_Readout = X_Readout + X_StepSize;
-	if (X_Readout >= 12 * X_CPI || X_Readout <= -12 * X_CPI) X_StepSize = X_StepSize * -1;       // switch direction at endings
+	#if SCALE_X_TYPE == 0
+		SerialBT.print("X" + String(X_Readout) + ";" );
+		Serial.print("X" + String(X_Readout) + ";");
+		X_Readout = X_Readout + X_StepSize;
+		if (X_Readout >= 12 * X_CPI || X_Readout <= -12 * X_CPI) X_StepSize = X_StepSize * -1;             // switch direction at endings
+	#endif
+	#if SCALE_X_TYPE == 1
+		while (digitalRead(SCALE_X_CLK_PIN) == LOW) {} // If clock is LOW wait until it turns to HIGH
+		time_now = micros();
+		while (digitalRead(SCALE_X_CLK_PIN) == HIGH) {} // Wait for the end of the HIGH pulse
+		if ((micros() - time_now) > 500) { // If the HIGH pulse was longer than 500 micros we are at the start of a new bit sequence
+		decode(); //decode the bit sequence
+		}
+	#endif
 #endif
 
 #if SCALE_Y_ENABLED == 1
 	SerialBT.print("Y" + String(Y_Readout) + ";" );
 	Serial.print("Y" + String(Y_Readout) + ";");
 	Y_Readout = Y_Readout + Y_StepSize;
-	if (Y_Readout >= 12 * Y_CPI || Y_Readout <= -12 * Y_CPI) Y_StepSize = Y_StepSize * -1;      // switch direction at endings
+	if (Y_Readout >= 12 * Y_CPI || Y_Readout <= -12 * Y_CPI) Y_StepSize = Y_StepSize * -1;             // switch direction at endings
 #endif
 
 #if SCALE_Z_ENABLED == 1
 	SerialBT.print("Z" + String(Z_Readout) + ";" );
 	Serial.print("Z" + String(Z_Readout) + ";");
 	Z_Readout = Z_Readout + Z_StepSize;
-	if (Z_Readout >= 12 * Z_CPI || Z_Readout <= -12 * Z_CPI) Z_StepSize = Z_StepSize * -1;      // switch direction at endings
+	if (Z_Readout >= 12 * Z_CPI || Z_Readout <= -12 * Z_CPI) Z_StepSize = Z_StepSize * -1;             // switch direction at endings
 #endif
 
 #if SCALE_W_ENABLED == 1
 	SerialBT.print("W" + String(W_Readout) + ";" );
 	Serial.print("W" + String(W_Readout) + ";");
 	W_Readout = W_Readout + W_StepSize;
-	if (W_Readout >= 360 * W_CPA || W_Readout <= 0) W_StepSize = W_StepSize * -1;               // switch direction at endings
+	if (W_Readout >= 360 * W_CPA || W_Readout <= 0) W_StepSize = W_StepSize * -1;             // switch direction at endings
 #endif
 
 #if TACH_ENABLED == 1
 	SerialBT.print("T" + String(T_Readout) + ";" );
 	Serial.print("T" + String(T_Readout) + ";");
 	T_Readout = T_Readout + T_StepSize;
-	if (T_Readout >= TACH_MAX_SPEED || T_Readout <= 0) T_StepSize = T_StepSize * -1;            // switch direction at endings
+	if (T_Readout >= TACH_MAX_SPEED || T_Readout <= 0) T_StepSize = T_StepSize * -1;             // switch direction at endings
 #endif
 
 	Serial.println("");					// Newline
@@ -185,3 +213,38 @@ void loop()
 
 }
 
+void decode() {
+	int sign = 1;
+	int i = 0;
+	float value = 0.0;
+	float result = 0.0;
+	bit_array[i] = digitalRead(SCALE_X_DATA_PIN); // Store the 1st bit (start bit) which is always 1.
+	while (digitalRead(SCALE_X_CLK_PIN) == HIGH) {};
+	for (i = 1; i <= 24; i++) {
+		while (digitalRead(SCALE_X_CLK_PIN) == LOW) {} // Wait until clock returns to HIGH
+		bit_array[i] = digitalRead(SCALE_X_DATA_PIN);
+		while (digitalRead(SCALE_X_CLK_PIN) == HIGH) {} // Wait until clock returns to LOW
+	}
+	for (i = 0; i <= 24; i++) { // Show the content of the bit array. This is for verification only.
+		Serial.print(bit_array[i]);
+		Serial.print(" ");
+	}
+	Serial.println();
+	for (i = 1; i <= 20; i++) { // Turning the value in the bit array from binary to decimal.
+		value = value + (pow(2, i - 1) * bit_array[i]);
+	}
+	if (bit_array[21] == 1) sign = -1; // Bit 21 is the sign bit. 0 -> +, 1 => -
+	if (bit_array[24] == 1) { // Bit 24 tells the measureing unit (1 -> in, 0 -> mm)
+		result = (value*sign) / 2000.00;
+		SerialBT.print("X" + String(result) + ";");
+		Serial.print(result, 3); // Print result with 3 decimals
+		Serial.println(" in");
+	}
+	else {
+		result = (value*sign) / 100.00;
+		SerialBT.print("X" + String(result) + ";");
+		Serial.print(result, 2); // Print result with 2 decimals
+		Serial.println(" mm");
+	}
+	//delay(10);
+}
